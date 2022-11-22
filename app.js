04bb59968acd098cedadd9a823b8a471aa7ad098
graphoeil <graphoeil@gmail.com>
2022-11-22 15:08:28 +0100

    fs module (sync)

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // Built-in modules
-// Path module
+// FS module - sync
 
+// Can do => const fs = require('fs'); => fs.readFileSync ...
+const { readFileSync, writeFileSync } = require('fs');
+
+// Read files
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf-8');
+console.log(first, second); // Hello this is the first text file ! Hello this is the second text file !
+
+// Create file
+// If file doesn't exist, node will create it, otherwise node will overwrite it !
+// To append to the file we need to pass a third argument => flag:'a'
+writeFileSync('./content/result-sync.txt', `Here is the result : ${ first }, ${ second }`, {
+	flag:'a'
+});
